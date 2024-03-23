b4b8cb24c604ca0227f46ddbdaf1a5ec77ad4ece
hibhad <h.ibhade69@gmail.com>
2024-03-23 19:09:26 +0000

    Latest version

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,27 +2,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.25.0"
+      version = "~> 5.25.0"   # Latest version as of now
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~> 3.5.1"    # Latest version as of now
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.0.4"    # Latest version as of now
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      version = "~> 2.3.2"    # Latest version as of now
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
+      version = "~> 2.23.0"   # Latest version as of now
     }
   }
 
@@ -32,8 +32,7 @@ terraform {
     region = "us-east-1"
   }
 
-  # required_version = "~> 1.6.6"
-    required_version = "~> 1.7.5"
+  required_version = "~> 1.7.5"   # Latest version as of now
 }
 
 
