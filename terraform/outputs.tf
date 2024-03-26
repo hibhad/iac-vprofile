5c809759cef4299a43b686ba26a7dec8c495b0df
hibhad <h.ibhade69@gmail.com>
2024-03-26 02:12:27 +0000

    Outcomes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,7 +14,7 @@ output "region" {
 }
 
 
-output "cluster_security_group_id" {
+Output "cluster_security_group_id" {
   description = "Security group ID for the Amazon Web Service EKS Cluster "
   value       = module.eks.cluster_security_group_id
 }
